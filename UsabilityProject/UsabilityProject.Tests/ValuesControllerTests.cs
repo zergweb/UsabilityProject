aaf3749010f3d0af60eabef8cdc3c4485813f52c
zergweb <zergwebprot@gmail.com>
2018-10-17 01:00:40 +0300

    add tests

---
+++ UsabilityProject/UsabilityProject.Tests/ValuesControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UsabilityProject.Controllers;
+using Xunit;
+
+namespace UsabilityProject.Tests
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public void GetResultNotNull()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+            // Act
+            IEnumerable<string> result = controller.Get() as IEnumerable<string>;
+            // Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void IndexViewDataMessage()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            IEnumerable<string> result = controller.Get() as IEnumerable<string>;
+
+            // Assert
+            Assert.Equal(new string[]{ "value1", "value2" }, result);
+        }
+
+    }
+}
